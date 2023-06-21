f8e571ed3d832f61ebf222ed9fdfba33f96eaf36
Gova <govaway@gmail.com>
2023-06-21 12:34:49 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const navBar = document.querySelector('.navbar');
+const navList = document.querySelector('.nav-lists');
+const dropDown = documnet.querySelector('.dropdown');
+const dropDownMenu = document.querySelector('.dropdown-menu');
+const burgerIcon = document.querySelector('.burger');
+const 
