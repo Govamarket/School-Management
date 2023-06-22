f4b4c130eba00f38d2b431fb7ece1f5511e62d0f
Gova <govaway@gmail.com>
2023-06-22 10:36:53 +0100

    Initial commit

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-const navBar = document.querySelector('.navbar');
-const navList = document.querySelector('.nav-lists');
-const dropDown = documnet.querySelector('.dropdown');
-const dropDownMenu = document.querySelector('.dropdown-menu');
-const burgerIcon = document.querySelector('.burger');
-const 
+const navBar = document.querySelector(".navbar");
+const navList = document.querySelector(".nav-lists");
+const dropDown = documnet.querySelector(".dropdown");
+const dropDownMenu = document.querySelector(".dropdown-menu");
+const burgerIcon = document.querySelector(".burger");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const studentId = document.querySelector("#student");
+const passWord = document.querySelector('#psw');
+const password2 = document.querySelector('#psw2');
+const buttonSubmit = document.querySelector('#submit');
+
