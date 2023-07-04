b4a2a3aeca9b9a7538930cbe356c870761e58ffc
Gova <govaway@gmail.com>
2023-07-04 15:33:31 +0100

    initial commit

--- register/register.js
+++ register/register.js
@@ -1,20 +1,33 @@
-  const surName = document.querySelector("#surname");
-  const middleName = document.querySelector("#middlename");
-  const emailInput = document.querySelector("email");
-  const generatId = document.querySelector("#studentid");
-  const password = document.querySelector("#pswd");
-  const passwordConfirmation = document.querySelector("#pswd2");
-  const signUp = document.querySelector("#signup");
-  const singIn = document.querySelector("#signin");
-  const inputFunction = document.querySelectorAll("input");
-  const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
+import{
+  
+}
 // Inputs main validation function to validate
 const inputSurnaValid = () => {
   if (surName.value === "") {
       alert("Please enter surname");
+
   }
 };
+
+
+inputSurnaValid();
+
+const inputName = (name) => {
+  if (middleName.value === "") {
+    alert("please enter name");
+  }
+}
+
+inputName();
+
+const emailValid = () => {
+  if (emailInput.value === "" || !emailInput.value ===  validRegex.value) {
+    alert("Please enter");
+  }
+}
+
+emailValid();
+
 
 //Targeting error messages via to appear in (Using this method for now)
 
@@ -25,8 +38,17 @@ surName.addEventListener("change", (event) => {
   }, 2000);
 });
 
+middleName.addEventListener("change", (event) => {
+  setTimeout(() => {
+    inputName();
+  }, 2000);
+}
 
-inputSurnaValid();
+emailInput.addEventListener("change", (event) => {
+
+});
+
+
 
 
 
--- variables.js
+++ variables.js
@@ -1,1 +1,12 @@
 // using javascript to create html text nodes will be highlighted soon after
+
+const surName = document.querySelector("#surname");
+const middleName = document.querySelector("#middlename");
+const emailInput = document.querySelector("email");
+const generatId = document.querySelector("#studentid");
+const password = document.querySelector("#pswd");
+const passwordConfirmation = document.querySelector("#pswd2");
+const signUp = document.querySelector("#signup");
+const singIn = document.querySelector("#signin");
+const inputFunction = document.querySelectorAll("input");
+const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
